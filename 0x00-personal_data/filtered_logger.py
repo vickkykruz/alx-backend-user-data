16564a8fcef666140a3311d4c031e851325f473e
Edward Victor Chukwuemeka <onwuegbuchulemvic02@gmail.com>
2024-04-10 20:59:04 +0000

    testing

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/env python3
 """ This is a module that call the function filter_datum that returns
 the log message obfuscated:
@@ -5,11 +6,15 @@ the log message obfuscated:
 
 
 import re
+from typing import List
 
 
-def filter_datum(fields, redaction, message, separator):
-    """ This is rwturn the value of the paaaes parameter """
-
-    return re.sub(
-            rf"(?:{separator})({'|'.join(fields)})=(.*?){separator}",
-            rf"\1={redaction}\2", message)
+def filter_datum(fields: List[str],
+                 redaction: str,
+                 message: str,
+                 separator: str) -> str:
+    """ Thia function returns the log message obfuscated """
+    for field in fields:
+        message = re.sub(f"{field}=.*?{separator}",
+                         f"{field}={redaction + separator}", message)
+    return message
