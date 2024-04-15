799e1932ea0593abca64ae994534ebce2604935b
Edward Victor Chukwuemeka <onwuegbuchulemvic02@gmail.com>
2024-04-15 12:12:27 +0000

    Basic - Base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -13,13 +13,26 @@ class BasicAuth(Auth):
         """ Extract the Base64 part of the Authorization header for
             Basic Authentication.
         """
-            
+
         if authorization_header is None or not isinstance(
                 authorization_header, str):
             return None
-            
+
         if not authorization_header.startswith('Basic '):
             return None
-            
+
         return authorization_header.split(' ')[1]
 
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """ Decode the Base64 string and return the decoded value as UTF-8
+        string."""
+        if base64_authorization_header is None or not isinstance(
+                base64_authorization_header, str):
+            return None
+
+        try:
+            decoded_bytes = base64.b64decode(base64_authorization_header)
+            return decoded_bytes.decode('utf-8')
+        except Exception:
+            retun None
