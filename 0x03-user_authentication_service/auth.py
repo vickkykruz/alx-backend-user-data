d94c428834df43ccb575ce3b7e9d7ccf642241bf
Edward Victor Chukwuemeka <onwuegbuchulemvic02@gmail.com>
2024-04-23 12:07:02 +0000

    Credentials validation

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -2,7 +2,7 @@
 """ This is a module that handle the authenication of the user """
 
 
-from bcrypt import hashpw, gensalt
+from bcrypt import hashpw, gensalt, checkpw
 from db import DB
 from sqlalchemy.orm.exc import NoResultFound
 from user import User
@@ -39,3 +39,20 @@ class Auth:
             raise ValueError(f"User {email} already exists")
         except NoResultFound:
             return self._db.add_user(email, _hash_password(password))
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """ validates the user credentials
+
+        Args:
+            email (str): email of the user
+            password (str): password of the user
+
+        Return:
+            True: valid user
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+        except Exception:
+            return False
+
+        return checkpw(password.encode('utf-8'), user.hashed_password)
