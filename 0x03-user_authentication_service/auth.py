f61d83f5e689bc38a5d4514d3a2a32090d6128c8
Edward Victor Chukwuemeka <onwuegbuchulemvic02@gmail.com>
2024-04-23 13:39:33 +0000

    Find user by session ID

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -84,3 +84,16 @@ class Auth:
             session_id = _generate_uuid()
             self._db.update_user(user.id, session_id=session_id)
             return session_id
+
+    def get_user_from_session_id(self, session_id: str) -> User:
+        """ Get the user object
+        Args:
+            session_id (str): the string representation of the session_id
+        Returns:
+            User object or None
+        """
+        if session_id:
+            try:
+                return self._db.find_user_by(session_id=session_id)
+            except Exception:
+                return None
