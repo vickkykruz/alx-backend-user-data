6f22479e0b6d50eb16a09e5df208b9724bb0b8da
Edward Victor Chukwuemeka <onwuegbuchulemvic02@gmail.com>
2024-04-23 13:27:48 +0000

    Log in

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,7 +2,7 @@
 """ This is a module that handle the Flask app """
 
 
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, make_response, abort
 from auth import Auth
 
 app = Flask(__name__)
@@ -30,6 +30,21 @@ def register_user() -> str:
         return jsonify({"email": email, "message": "user created"}), 200
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login() -> str:
+    """ logs in a user """
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    if not AUTH.valid_login(email, password):
+        abort(401)
+
+    session_id = AUTH.create_session(email)
+    response = make_response({"email": email, "message": "logged in"})
+    response.set_cookie('session_id', session_id)
+    return response
 
 
 if __name__ == "__main__":
